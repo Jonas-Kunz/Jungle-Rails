c961d20dadea1b36e77123b7871b356036076e62
Jonas-Kunz <jonas_K_7@outlook.com>
2024-03-11 16:46:12 -0300

    Add test to visit home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -8,4 +8,9 @@ describe('example to-do app', () => {
   it('displays the home page', () => {
     cy.get(".products article").should("be.visible");
   })
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+  
 })
